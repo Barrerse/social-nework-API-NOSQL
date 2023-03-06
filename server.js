52cc5afa7067c57366424dfe97600a565a2f53e3
Barrerse <sebastianbarrera@live.com>
2023-03-06 10:04:50 -0500

    fixed connection route on server.js

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const db = require("./config/conneciton")
-const routes = require("./routes");
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
 
-const app = express();
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(routes);
 
-db.once("open", () => {
+db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
