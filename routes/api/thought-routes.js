9f4368cde2d3afb2d896b5dbdec1196b2510d418
Barrerse <sebastianbarrera@live.com>
2023-03-06 09:57:23 -0500

    renamed from controller form comment to thought now to Post in order to keep it simple and follow code easier. Also edited code to make it function

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -7,7 +7,7 @@ const {
   deleteThought,
   addReaction,
   removeReaction,
-} = require("../../controllers/thought-controller");
+} = require("../../controllers/Post-Controller");
 
 // /api/thoughts
 router.route("/").get(getAllThought).post(createThought);
